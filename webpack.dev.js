72c563cda569ceba18aa1a970ef245c13546fce9
SalahAmer2 <salahayman2@gmail.com>
2020-03-17 21:19:12 +0200

    trying to make background image display on webpage

--- webpack.dev.js
+++ webpack.dev.js
@@ -57,6 +57,6 @@ module.exports = {
     new CopyPlugin([
       { from: 'source', to: 'dest' },
       { from: 'other', to: 'public' },
-    ]),
+    ])
   ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,36 +5,54 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    optimization: {
-      minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
-    },
-    output: {
-      libraryTarget: 'var',
-      library: 'Client'
-    },
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-              test: /\.scss$/,
-              use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+  entry: './src/client/index.js',
+  mode: 'production',
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  output: {
+    libraryTarget: 'var',
+    library: 'Client'
+  },
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/'
             }
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new MiniCssExtractPlugin({filename: '[name].css'}),
-        new WorkboxPlugin.GenerateSW()
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+    new WorkboxPlugin.GenerateSW(),
+    new CopyPlugin([
+      { from: 'source', to: 'dest' },
+      { from: 'other', to: 'public' },
+    ])
+  ]
 }
