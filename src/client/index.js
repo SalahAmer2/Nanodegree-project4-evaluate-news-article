413cf566840b0d697b7840e30d2c5134ddbdb80f
SalahAmer2 <salahayman2@gmail.com>
2020-03-17 04:01:21 +0200

    commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,6 @@
 //jshint esversion:9
-// import { validateURL } from './js/validateURL'
 import { handleSubmit } from './js/formHandler';
 import { isValidURL } from './js/validateURL';
-// import { isValidURL } from '../../_tests_/validateURL.spec';
 import './styles/resets.scss';
 import './styles/header.scss';
 import './styles/form.scss';
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,18 +1,18 @@
-//jshint esversion: 9
+// jshint esversion: 9
 // import {validateURL} from './validateURL';
 
-document.getElementById("submit").addEventListener('click', handleSubmit);
+document.getElementById('submit').addEventListener('click', handleSubmit)
 
-function handleSubmit(event) {
-    event.preventDefault();
+function handleSubmit (event) {
+  event.preventDefault()
 
     // check what text was put into the form field
     // let formText = document.getElementById('name').value;
     // checkForName(formText);
 
-    console.log("::: Form Submitted :::");
-    const baseURL = "http://localhost:8080/sentiment";
-    let inputtedUrl = document.getElementById('url').value;
+  console.log('::: Form Submitted :::')
+  const baseURL = 'http://localhost:8080/sentiment'
+  let inputtedUrl = document.getElementById('url').value
 
     // const isValidURL = (userInput) => {
     //   var res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
@@ -22,33 +22,31 @@ function handleSubmit(event) {
     //       return true;
     // };
 
-    if(Client.isValidURL(inputtedUrl)){
-      fetch(baseURL, {//fetches from app.post("/sentiment"
-        method: "POST",
-        credentials: 'same-origin',
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ url: inputtedUrl })
-      })
-        .then(res => res.json())//res is what's received from server/index.js from res.send(projectData);
+  if (Client.isValidURL(inputtedUrl)) { // thinking of using toBeTruthy in its jest
+    fetch(baseURL, {// fetches from app.post("/sentiment"
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ url: inputtedUrl })
+    })
+        .then(res => res.json())// res is what's received from server/index.js from res.send(projectData);
         .then(res => {
           document.getElementById(
-            "polarity"
-          ).innerHTML = `<strong>Polarity:</strong><br> ${res.polarity}`;
+            'polarity'
+          ).innerHTML = `<strong>Polarity:</strong><br> ${res.polarity}`
           document.getElementById(
-            "subjectivity"
-          ).innerHTML = `<strong>Subjectivity:</strong><br> ${res.subjectivity}`;
-          document.getElementById("text").innerHTML = `<p>${res.text}</p>`;
-        });
-    } else {
-      alert("Invalid URL");
-    }
+            'subjectivity'
+          ).innerHTML = `<strong>Subjectivity:</strong><br> ${res.subjectivity}`
+          document.getElementById('text').innerHTML = `<p>${res.text}</p>`
+        })
+  } else {
+    alert('Invalid URL')
+  }
 }
 
-export { handleSubmit };
-
-
+export { handleSubmit }
 // const res = await fetch('http://localhost:8080/sentiment')
 
 // .then(res => res.json())
